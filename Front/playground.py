f637e64b3c7dca7766276f66529b455c087b2f16
ProgrammistDebilov <nikita2006tv@gmail.com>
2023-04-18 22:14:21 +1100

    minor changes

--- Front/playground.py
+++ Front/playground.py
@@ -1,16 +1,6 @@
 import folium
+import webbrowser
 
-# Import folium MarkerCluster plugin
-from folium.plugins import MarkerCluster
-# Import folium MousePosition plugin
-from folium.plugins import MousePosition
-# Import folium DivIcon plugin
-from folium.features import DivIcon
-
-# define the world map
-world_map = folium.Map()
-
-# display world map
-world_map
-
-#test
+m = folium.Map()
+m.save("map.html")
+webbrowser.open("map.html")
