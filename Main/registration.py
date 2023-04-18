76a80f85bfe95896d242fc6df5156cb50988141a
XRenso <tch.xr@ya.ru>
2023-04-18 22:45:21 +1100

    tipo togo

--- Main/registration.py
+++ Main/registration.py
@@ -2,8 +2,8 @@ import flet as ft
 
 def main(page: ft.Page):
 
-    Title = ft.Text('Установка',size=36)
-    t= ft.Column([Title])
+    Title = ft.Text('INSTALL MANAGER',size=36, color=ft.colors.PINK)
+    t= ft.Column([Title],alignment=ft.MainAxisAlignment.CENTER)
     c = ft.Container(content=t,width=page.width,height=page.height, alignment=ft.alignment.center)
     def change_size(e):
         c.width = page.width
