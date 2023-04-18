350c57ca42f46781992d9efcdb19e0c1d6384768
XRenso <tch.xr@ya.ru>
2023-04-18 23:03:46 +1100

    tipo togo

--- registration.py
+++ registration.py
@@ -1,8 +1,11 @@
+import time
+
 import flet as ft
 
 def main(page: ft.Page):
+    Rosstelecom_logo = ft.Image(src='/images/r_logo.png', width=300,fit=ft.ImageFit.CONTAIN)
 
-    Title = ft.Text('INSTALL MANAGER',size=36, color=ft.colors.PINK)
+    Title = ft.Row([Rosstelecom_logo],alignment=ft.MainAxisAlignment.CENTER)
     t= ft.Column([Title],alignment=ft.MainAxisAlignment.CENTER)
     c = ft.Container(content=t,width=page.width,height=page.height, alignment=ft.alignment.center)
     def change_size(e):
@@ -12,5 +15,6 @@ def main(page: ft.Page):
     page.add(c)
     page.on_resize = change_size
     page.update()
+    time.sleep(1)
 
-ft.app(target=main, view=ft.WEB_BROWSER)
+ft.app(target=main, view=ft.WEB_BROWSER, assets_dir='assets')
