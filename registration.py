0869e731574ab3d96eaffb0659378037929584de
XRenso <tch.xr@ya.ru>
2023-04-19 00:36:27 +1100

    Super update

--- registration.py
+++ registration.py
@@ -13,11 +13,34 @@ def main(page: ft.Page):
                 pass
         else:
             is_exist_acc.label = 'Я ещё не смешарик'
+            enter_btn.text = 'Зарегистрироваться'
             if role_choose not in login_content:
                 login_content.insert(3,role_choose)
-            enter_btn.text = 'Зарегистрироваться'
+
         page.update()
 
+    def enter_btn_action(e):
+        if is_exist_acc: #логин
+            brak = False
+            if login.value == '':
+                login.error_text = 'Вы забыли заполнить'
+                login.update()
+                brak = True
+            else:
+                login.error_text = None
+                login.update()
+            if password.value == '':
+                password.error_text = 'У вас обязан быть пароль'
+                password.update()
+                brak = True
+            else:
+                password.error_text = None
+                password.update()
+            if not brak:
+                print('Succes')
+
+        else:#регистрация
+            pass
 
     Title = ft.Image(src='/images/r_logo.png',width=150,fit=ft.ImageFit.CONTAIN)
     login = ft.TextField(label='Логин', hint_text='Введите ваш логин',width=300,focused_border_color='#7C4DFF')
@@ -31,17 +54,22 @@ def main(page: ft.Page):
         ],
         focused_border_color='#7C4DFF'
     )
-    enter_btn = ft.ElevatedButton('Зарегистроваться',bgcolor='#ff4f12', color=ft.colors.WHITE)
+    enter_btn = ft.ElevatedButton('Зарегистроваться',bgcolor='#ff4f12', color=ft.colors.WHITE, on_click=enter_btn_action)
     is_exist_acc = ft.Checkbox(label='Я ещё не смешарик', value=False, on_change=is_exist_acc_change)
 
     page.bgcolor = '#37474F'
+
     login_content = [Title,login,password,role_choose,enter_btn,is_exist_acc]
+
+
+
     t= ft.Column(login_content,alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)
     c = ft.Container(content=t,width=page.width,height=page.height, alignment=ft.alignment.center)
     def change_size(e):
         c.width = page.width
         c.height = page.height
         page.update()
+
     page.add(c)
     page.on_resize = change_size
     page.update()
