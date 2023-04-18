b989d7b7e7ba612412010009062f3f82ac1c8e33
Ivan <ivanu1706@gmail.com>
2023-04-18 23:56:14 +1100

    регистрация и авторизация

--- Backend/work_db.py
+++ Backend/work_db.py
@@ -13,8 +13,26 @@ def sign_up(options):
         role = 'i'
     elif role == 'диспетчер':
         role = 'd'
+    row = cur.execute(f'SELECT * FROM users WHERE login = {login}')
 
-    cur.execute(
-        f'INSERT INTO users (name, surname, login, password, role) VALUES({name}, {surname}, {login}, {password}, {role});')
+    if row:
+        print('Не не не, этот логин уже используется')
+    else:
+        cur.execute(
+            f'INSERT INTO users (name, surname, login, password, role) VALUES({name}, {surname}, {login}, {password}, {role});')
 
     con.commit()
+
+def sign_in(options):
+    con = sql3.connect('installs.db')
+    cur = con.cursor()
+
+    login = options[0]
+    pasword = options[1]
+
+    row = cur.execute(f'SELECT * FROM users WHERE login = {login} AND WHERE password = {pasword}')
+
+    if row:
+       print('good')
+    else:
+        print('bad')
