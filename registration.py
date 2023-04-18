11c345da780c281a68efc424c6b0388d7e7ecd2a
XRenso <tch.xr@ya.ru>
2023-04-18 23:41:40 +1100

    tipo togo

--- registration.py
+++ registration.py
@@ -3,6 +3,14 @@ import time
 import flet as ft
 
 def main(page: ft.Page):
+    def is_exist_acc_change(e):
+        if is_exist_acc.value:
+            is_exist_acc.label = 'Я уже смешарик'
+        else:
+            is_exist_acc.label = 'Я ещё не смешарик'
+        page.update()
+
+
     Title = ft.Image(src='/images/r_logo.png',width=150,fit=ft.ImageFit.CONTAIN)
     login = ft.TextField(label='Логин', hint_text='Введите ваш логин',width=300,focused_border_color='#7C4DFF')
     password = ft.TextField(label='Пароль', hint_text='Введите ваш пароль',width=300,focused_border_color='#7C4DFF')
@@ -16,9 +24,10 @@ def main(page: ft.Page):
         focused_border_color='#7C4DFF'
     )
     enter_btn = ft.ElevatedButton('Зарегистроваться',bgcolor='#ff4f12', color=ft.colors.WHITE)
+    is_exist_acc = ft.Checkbox(label='Я ещё не смешарик', value=False, on_change=is_exist_acc_change)
 
     page.bgcolor = '#37474F'
-    login_content = [Title,login,password,role_choose,enter_btn]
+    login_content = [Title,login,password,role_choose,is_exist_acc,enter_btn]
     t= ft.Column(login_content,alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)
     c = ft.Container(content=t,width=page.width,height=page.height, alignment=ft.alignment.center)
     def change_size(e):
