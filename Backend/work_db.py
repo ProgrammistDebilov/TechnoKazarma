a9c2515094c52e3f9b658b961662d91ec2c6af98
Ivan <ivanu1706@gmail.com>
2023-04-18 23:32:35 +1100

    регистрация

--- Backend/work_db.py
+++ Backend/work_db.py
@@ -1,2 +1,18 @@
+import sqlite3 as sql3
+
 def sign_up(options):
-    pass
+    con = sql3.connect('installs.db')
+    cur = con.cursor()
+
+    name = options[0]
+    surname = options[1]
+    login = options[2]
+    password = options[3]
+    role = options[4]
+    if role == 'инсталятор':
+        role = 'i'
+    elif role == 'диспетчер':
+        role = 'a'
+
+    cur.execute(
+        f'INSERT INTO users (name, surname, login, password, role) VALUES({name}, {surname}, {login}, {password}, {role});')
