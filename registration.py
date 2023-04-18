46a56fbaf062fd20b61c6b4e0578562eab047c63
XRenso <tch.xr@ya.ru>
2023-04-18 23:35:26 +1100

    tipo togo

--- registration.py
+++ registration.py
@@ -4,15 +4,22 @@ import flet as ft
 
 def main(page: ft.Page):
     Title = ft.Image(src='/images/r_logo.png',width=150,fit=ft.ImageFit.CONTAIN)
+    login = ft.TextField(label='Логин', hint_text='Введите ваш логин',width=300,focused_border_color='#7C4DFF')
+    password = ft.TextField(label='Пароль', hint_text='Введите ваш пароль',width=300,focused_border_color='#7C4DFF')
     role_choose = ft.Dropdown(
-        width=200,
+        label='Роль',
+        width=155,
         options=[
             ft.dropdown.Option('Диспетчер'),
             ft.dropdown.Option('Инсталятор')
-        ]
+        ],
+        focused_border_color='#7C4DFF'
     )
+    enter_btn = ft.ElevatedButton('Зарегистроваться',bgcolor='#ff4f12', color=ft.colors.WHITE)
+
     page.bgcolor = '#37474F'
-    t= ft.Column([Title,role_choose],alignment=ft.MainAxisAlignment.CENTER)
+    login_content = [Title,login,password,role_choose,enter_btn]
+    t= ft.Column(login_content,alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)
     c = ft.Container(content=t,width=page.width,height=page.height, alignment=ft.alignment.center)
     def change_size(e):
         c.width = page.width
