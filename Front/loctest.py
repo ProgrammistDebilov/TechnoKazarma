8296d7159536400bd648f7e451091d8d6cb6ced2
ProgrammistDebilov <nikita2006tv@gmail.com>
2023-04-20 18:28:28 +1100

    added login

--- Front/loctest.py
+++ Front/loctest.py
@@ -12,6 +12,7 @@ app = dash.Dash(__name__)
 # Define the initial marker position
 logins = []
 locations = []
+login = ""
 for i in db.return_installers():
         logins.append(i['login'])
 for i in logins:
@@ -28,6 +29,7 @@ print(markers)
 # Define the layout of the app
 app.layout = html.Div([
     Map(markers, style={'width': 'auto', 'height': '98vh', 'margin': "auto", "display": "block"}, zoom=3),
+    html.Div(id="text"),
     dcc.Interval(
         id='interval',
         interval=2000, # Refresh every 2 seconds
@@ -35,9 +37,14 @@ app.layout = html.Div([
     )
 ])
 
+def test(l):
+    global login
+    login = l
+
 # Define a callback to update the marker position
 
-@app.callback(Output('marker', 'position'),Output('marker2', 'position'), [Input('interval', 'n_intervals'), Input('marker', 'children'), Input('marker2', 'children')])
+@app.callback(Output('marker', 'position'),Output('marker2', 'position'), Output('text', 'children') [Input('interval', 'n_intervals'), Input('marker', 'children'), Input('marker2', 'children')])
+
 def update_marker_position(n, k, j):
     print(k['props']['children'], j['props']['children'])
     logins.clear()
@@ -45,9 +52,9 @@ def update_marker_position(n, k, j):
         logins.append(i['login'])
     for i in logins:
         print(list(db.return_location(i)))
-    return [[str(random.randint(-90,90)), str(random.randint(-180,180))],[random.randint(-90,90), random.randint(-180,180)]]
-def test(a):
-    print(a)
+    return [[str(random.randint(-90,90)), str(random.randint(-180,180))],[random.randint(-90,90), random.randint(-180,180)], login]
+
+
 
 # Run the app
 if __name__ == '__main__':
