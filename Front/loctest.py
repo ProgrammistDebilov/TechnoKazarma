b53eeac1a986c4fa48445b639e9d5a6ebfee3e30
ProgrammistDebilov <nikita2006tv@gmail.com>
2023-04-20 21:46:42 +1100

    added geosync

--- Front/loctest.py
+++ Front/loctest.py
@@ -1,52 +1,60 @@
 from dash import *
-from dash_leaflet import Marker, TileLayer, Map, Tooltip
+from dash_leaflet import Marker, TileLayer, Map, Tooltip, LocateControl
 from dash.dependencies import Output, Input
 import Backend.work_db as db
 
 # Create a Dash app
 app = dash.Dash(__name__)
 
-# Define the initial marker position
+
 logins = []
 locations = []
-login = "здесь ничего"
+returns = []
+login = "логин не передался"
+callbacks = Output("text", "children")
 for i in db.return_installers():
         logins.append(i['login'])
 for i in logins:
     locations.append(list(db.return_location(i)))
-# Define a list of marker positions to loop through
-markers = [TileLayer()]
+
+# Define a list of markers
+markers = [TileLayer(), LocateControl(options={'locateOptions': {'enableHighAccuracy': True}})]
 for i, j in enumerate(locations):
     markers.append(Marker(id = str(i), position=list(j), children=Tooltip(logins[i])))
-# Define a list of marker positions to loop through
+
 
 
 # Define the layout of the app
+app = Dash(external_stylesheets=['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
+                prevent_initial_callbacks=True)
 app.layout = html.Div([
-    Map(markers, style={'width': 'auto', 'height': '50vh', 'margin': "auto", "display": "block"}, zoom=3, id= 'map'),
+    Map(markers, style={'width': 'auto', 'height': '95vh', 'margin': "auto", "display": "block"}, zoom=3, id= 'map'),
     dcc.Interval(
         id='interval',
         interval=2000, # Refresh every 2 seconds
         n_intervals=0
     ),
     html.Div(id="text"),
+    dcc.Location(id='url')
 ])
 
-def test(l):
-    global login
-    login = l
 
-# Define a callback to update the marker position
 
-@app.callback(Output("text", "children"), [Input('interval', 'n_intervals')])
+# Define a callback to update the markers position
 
-def update_marker_position(n):
+@app.callback(Output('text', 'children'), Input('interval', 'n_intervals'), Input('url', 'pathname'), Input("map", "location_lat_lon_acc"))
+def update_marker_position(n,k,j):
+    login = "Ваш логин: "+k[1::]
+    returns.clear()
     logins.clear()
     locations.clear()
     for i in db.return_installers():
         logins.append(i['login'])
     for i in logins:
-        list(db.return_location(i))
+        returns.append(list(db.return_location(i)))
+    if j != None:
+        db.insert_location(k[1::], j[0], j[1])
+    print(returns)
     return login
 
 
